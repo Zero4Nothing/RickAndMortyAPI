b2a80470879f56cdc470838a5e01741e0bc33dce
unknown <SENA@PALDFPCBIFSP233.FormacionSena.RED>
2023-03-18 15:16:52 -0500

    Main y navbar arreglado

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -23,15 +23,15 @@ function Navbar() {
             </ul>
         </div> */}
 
-      <nav
+      {/*   <nav
         className="navbar navbar-expand-lg bg-light text-bg-dark"
         aria-label="Eighth navbar example"
         style={{ backgroundColor: "#e3f2fd" }}
       >
         <div className="container d-flex">
           {/*<a class="navbar-brand" href="#">
-            Container</a>*/}
-          <img src={logo} alt="logo de Rick and Morty" />
+            Container</a>
+          <img className="mr-auto" src={logo} alt="logo de Rick and Morty" />
           <button
             className="navbar-toggler"
             type="button"
@@ -44,8 +44,55 @@ function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarsExample07">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div
+            className="justify-content-end collapse navbar-collapse"
+            id="navbarsExample07"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ml-auto ">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Personajes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Capítulos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Generador</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> 
+    
+    */}
+
+      <nav
+        className="navbar navbar-expand-lg bg-light text-bg-dark"
+        aria-label="Eighth navbar example"
+        style={{ backgroundColor: "#e3f2fd" }}
+      >
+        <div className="container d-flex align-items-center">
+          <img className="mr-auto" src={logo} alt="logo de Rick and Morty" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample07"
+            aria-controls="navbarsExample07"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            className="ml-auto justify-content-end collapse navbar-collapse"
+            id="navbarsExample07"
+          >
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
                   Personajes
