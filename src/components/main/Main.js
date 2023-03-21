28adea3fe6ba92bc4b1c4347dfc48803173554e4
Zero4Nothing <jose.zambrano556@misena.edu.co>
2023-03-20 23:37:57 -0500

    Background main colocado

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,23 +1,24 @@
 import React from "react";
-import fondo from "../../assets/BackgroundImgDesktop";
+import fondo from "./../../assets/BackgroundImgDesktop.png";
 
-function Home() {
+function Main() {
+  const styles = {
+    backgroundImage: `url(${fondo})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    height: "100vh",
+  };
+
   return (
-    <>
-      <main
-        class="px-3"
-        style={{
-          backgroundImage: `url(${fondo})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-          height: "100vh",
-        }}
-      >
+    <div>
+      <main className="px-3" style={styles}>
         <h1 className="text-center">
           El mejor lugar para ver tu serie favorita.
         </h1>
       </main>
-    </>
+    </div>
   );
 }
+
+export default Main;
